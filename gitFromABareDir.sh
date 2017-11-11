59e7b811594388f4130bea6a5497376233538697
xt57 <xt57dev@gmail.com>
2017-11-11 17:07:46 -0600

    removed personal data from the clone script

--- gitFromABareDir.sh
+++ gitFromABareDir.sh
@@ -2,11 +2,10 @@
 #
 #
 
-
-    git     config --global     user.name   xt57
+    git     config --global     user.name   <yourID>
     
 
-    git     config --global     user.email  "xt57dev@gmail.com"
+    git     config --global     user.email  <your email>
     
     
     git     clone   https://github.com/xt57/xt57seedAndCull.git
