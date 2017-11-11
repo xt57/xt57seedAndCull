61a852c90850cacb96a948923eafd4ef374bf0e9
xt57 <xt57dev@gmail.com>
2017-11-10 21:12:30 -0600

    updated the BareDir script

--- gitFromABareDir.sh
+++ gitFromABareDir.sh
@@ -3,10 +3,10 @@
 #
 
 
-    git     config --global     xt57    ag20253
+    git     config --global     user.name   xt57
     
 
-    git     config --global     xt57dev.gmail.com
+    git     config --global     user.email  "xt57dev@gmail.com"
     
     
     git     clone   https://github.com/xt57/xt57seedAndCull.git
