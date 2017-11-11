8a1f28bce4226f41a438942509d7ddb244d72b9f
xt57 <xt57dev@gmail.com>
2017-11-10 21:07:01 -0600

    adding a prototype script to manage clones to bare dirs

---
+++ gitFromABareDir.sh
@@ -0,0 +1,14 @@
+#
+#
+#
+
+
+    git     config --global     xt57    ag20253
+    
+
+    git     config --global     xt57dev.gmail.com
+    
+    
+    git     clone   https://github.com/xt57/xt57seedAndCull.git
+    
+    
