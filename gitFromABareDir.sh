a2a3a8d5d4637a1786be9eed017dee4b62e4ebea
xt57 <xt57dev@gmail.com>
2017-11-11 20:28:06 -0600

    added a git install command at the beginning of BareDir script

--- gitFromABareDir.sh
+++ gitFromABareDir.sh
@@ -2,12 +2,16 @@
 #
 #
 
-    git     config --global     user.name   <yourID>
+
+	sudo apt-get -y 	install git
+
+
+	git     config --global     user.name   xt57
     
 
-    git     config --global     user.email  <your email>
-    
-    
-    git     clone   https://github.com/xt57/xt57seedAndCull.git
+	git     config --global     user.email  xt57@gmail.com
+
+
+	git     clone   https://github.com/xt57/xt57seedAndCull.git
     
     
