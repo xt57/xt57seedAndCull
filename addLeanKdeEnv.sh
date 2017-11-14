566aa396f48c5bd93fc344547ec3f2f5e21c6919
xt57 <xt57dev@gmail.com>
2017-11-14 14:21:15 -0600

    added new script - addLeanKdeEnv.sh to ease lean/quick kde build

---
+++ addLeanKdeEnv.sh
@@ -0,0 +1,22 @@
+#
+#
+#
+
+    sudo    apt-get install      kde-plasma-desktop   --no-install-recommended
+
+    sudo    apt-get install     konsole
+
+    sudo    apt-get install     sddm
+
+
+    sudo    update              -y
+    sudo    upgrade             -y
+    sudo    dist-upgrade        -y
+
+    sudo    autoremove          -y
+    sudo    autoclean           -y
+
+    
+
+
+    
